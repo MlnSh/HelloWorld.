5ae9a3ab5979bfa82d998c205163dc549f7aab59
MlnSh <milanapags@gmail.com>
2024-11-04 14:21:34 +0300

    добавила js

--- script.js
+++ script.js
@@ -2,3 +2,40 @@ let a = '10';
 alert(a);
 a = '20';
 alert(a);
+let year = '2007';
+alert(year);
+let name = 'Brendan Eich';
+alert(name);
+let c = 10;
+let b = 2;
+let sum = c + b;
+let dif = c - b;
+let prod = c * b;
+let quo = c / b;
+alert(`${sum}, ${dif}, ${prod}, ${quo}`);
+let st = 2 ** 5;
+alert(st);
+let q = 9;
+let w = 2;
+let rem = q % w;
+alert(rem);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+let age = prompt("Сколько вам лет?")
+alert(age);
+
+const user = {
+    name: 'Mark',
+    age: 21,
+    'isAdmin': true
+}
+let userName = prompt("Ваше имя")
+alert('Привет' +', '+ userName)
