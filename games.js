21331a77ae32d9859bf7f1ede6202663b15ce638
MlnSh <milanapags@gmail.com>
2024-11-23 20:07:18 +0300

    игра 2

---
+++ games.js
@@ -0,0 +1,32 @@
+// игра 1
+const randomNum = Math.floor(Math.random() * 100) + 1;
+function game_1() {
+    // const num = Math.floor(Math.random() * 100) + 1;
+    let userTry = Number(prompt('угадай число от 1 до 100'));
+    if (userTry < randomNum) {
+        alert('не угадал, загаданное число больше');
+        game_1();
+    }
+    else if (userTry > randomNum) {
+        alert('не угадал, загаданное число меньше');
+        game_1();
+    }
+    else alert('поздравляю, угадал')
+}
+
+// игра 2
+
+function genRandomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function genTask() {
+    const randomNum_1 = genRandomNum(1, 100);
+    const operations = ['+', '-', '*', '/'];
+    const oper = operations[genRandomNum(0, operations.length - 1)];
+    const randomNum_2 = genRandomNum(1, 100);
+    let userAnswer = prompt(`Решите задачу: ${randomNum_1} ${oper} ${randomNum_2}`);
+    if (userAnswer != '${randomNum_1} ${oper} ${randomNum_2}' == false) {
+        alert('неверно'); }
+    else { alert('верно');
+    }
+}
