cda699890c9482e13fc1ac4b50784c4a8c8290ef
MlnSh <milanapags@gmail.com>
2024-12-20 07:44:28 +0300

    игра 1

--- games.js
+++ games.js
@@ -1,17 +1,20 @@
 // игра 1
 
-function game_1() {
+function game_one() {
     const randomNum = Math.floor(Math.random() * 100) + 1;
-    let userTry = Number(prompt('угадай число от 1 до 100'));
-    if (userTry < randomNum) {
+    function game_1() {
+        let userTry = Number(prompt('угадай число от 1 до 100'));
+        if (userTry < randomNum) {
         alert('не угадал, загаданное число больше');
         game_1();
     }
-    else if (userTry > randomNum) {
+        else if (userTry > randomNum) {
         alert('не угадал, загаданное число меньше');
         game_1();
     }
-    else alert('поздравляю, угадал')
+        else alert('поздравляю, угадал')
+    }
+    game_1()
 }
 
 // игра 2
